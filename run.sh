b739bbb9c5db226f5bd6969f859723f422e87f4e
Chernikava Alena <xcerni07@merlin.fit.vutbr.cz>
2014-12-14 13:56:42 +0100

    kript

--- run.sh
+++ run.sh
@@ -2,12 +2,15 @@
 
 FILES="graph_*"
 
-python ./graph/generator.py test
+#python ./graph/generator.py test
 
 for P in $FILES ; do
+    echo "$P" >> "thistest1"
     for A in 1 2 3 4 5 6 7 8 9 10 11 12; do
-        echo "$BASE"/"$P" with "$A"  threads
-        time taskset -c 0-12 ./parallel_v/matching "$P" "$A"
+	TIMEFORMAT='%3R'
+        { time taskset -c 0-4 ./parallel_v/matching "$P" "$A" >tothroug ; } 
+	echo ";" >&2 
+#        { time taskset -c 0-4 ./parallel_v/matching "$P" "$A" >tothroug ; } 
    done
 done
 
