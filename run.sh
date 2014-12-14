2228c38333fb9f2cbb81d204d96be4316b1d2319
Chernikava Alena <xcerni07@merlin.fit.vutbr.cz>
2014-12-14 14:45:38 +0100

    New version of test script

--- run.sh
+++ run.sh
@@ -5,11 +5,10 @@ FILES="graph_*"
 #python ./graph/generator.py test
 
 for P in $FILES ; do
-    echo "$P" >> "thistest1"
+    echo "$P" >> "$1"
     for A in 1 2 3 4 5 6 7 8 9 10 11 12; do
 	TIMEFORMAT='%3R'
-        { time taskset -c 0-4 ./parallel_v/matching "$P" "$A" >tothroug ; } 
-	echo ";" >&2 
+        { /usr/bin/time -f "%e;%M" -o "$1" -a taskset -c 0-12 ./parallel_v/matching "$P" "$A" >tothroug ; } 
 #        { time taskset -c 0-4 ./parallel_v/matching "$P" "$A" >tothroug ; } 
    done
 done
